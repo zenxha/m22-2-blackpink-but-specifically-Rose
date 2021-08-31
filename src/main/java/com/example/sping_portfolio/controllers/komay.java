42bc033a7e9c8b251152529b6966fd0d5a39b195
zenxha <komays5042@gmail.com>
2021-08-31 10:56:06 -0700

    fix

--- src/main/java/com/example/sping_portfolio/controllers/komay.java
+++ src/main/java/com/example/sping_portfolio/controllers/komay.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Greet3 {
-    @GetMapping("/tanmay")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="Tanmay") String name, Model model) {
+public class komay {
+    @GetMapping("/komay")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Anthony") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
-        return "tanmay"; // returns HTML VIEW (greeting)
+        return "komay"; // returns HTML VIEW (greeting)
     }
 }
