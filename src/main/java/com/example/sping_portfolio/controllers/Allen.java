ff2b66f4c572d19bab730aa4a67cecd4a38e3e92
zenxha <komays5042@gmail.com>
2021-09-16 10:09:27 -0700

    remove broken images

--- src/main/java/com/example/sping_portfolio/controllers/Allen.java
+++ src/main/java/com/example/sping_portfolio/controllers/Allen.java
@@ -16,39 +16,5 @@ public class Allen {
         model.addAttribute("name", name); // MODEL is passed to html
         return "rail"; // returns HTML VIEW (greeting)
     }
-    @GetMapping("/image")
-    public String image(Model model)  {
-        //String web_server = "http://localhost:8080/";
-        String web_server = "https://csa.nighthawkcodingsociety.com";
-        List<ImageInfo> lii = new ArrayList<>();
 
-        String file0 = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024";
-        lii.add(new ImageInfo(file0, web_server+file0, 12));
-        lii.get(0).read_image();
-
-        String file1 = "https://images2.minutemediacdn.com/image/upload/c_crop,h_1349,w_2400,x_0,y_138/v1628703164/shape/mentalfloss/649273-youtube-rick_astley.jpg?itok=8JRk0Avu";
-        lii.add(new ImageInfo(file1, web_server+file1, 2));
-        lii.get(1).read_image();
-
-        String file2 = "https://www.nme.com/wp-content/uploads/2021/07/RickAstley2021.jpg";
-        lii.add(new ImageInfo(file2, web_server+file2, 7));
-        lii.get(2).read_image();
-
-        model.addAttribute("lii", lii);
-        return "starters/image";
-    }
-
-    @GetMapping("/image/grayscale")
-    public String image_grayscale(Model model) {
-        //String web_server = "http://localhost:8080/";
-        String web_server = "https://csa.nighthawkcodingsociety.com";
-        List<ImageInfo> lii = new ArrayList<>();
-
-        String file0 = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1024";
-        lii.add(new ImageInfo(file0, web_server+file0, 12));
-        String str = lii.get(0).grayscale();
-//        String str = lii.get(0).grayscale();
-        model.addAttribute("str", str);
-        return "starters/image_grayscale";
-    }
 }
