7d36ab3d5d8ac5a18b4d84486f74357b573239f6
purplebears321 <32722440+purplebears321@users.noreply.github.com>
2021-09-16 10:03:54 -0700

    Update Allen.java

--- src/main/java/com/example/sping_portfolio/controllers/Allen.java
+++ src/main/java/com/example/sping_portfolio/controllers/Allen.java
@@ -16,4 +16,39 @@ public class Allen {
         model.addAttribute("name", name); // MODEL is passed to html
         return "rail"; // returns HTML VIEW (greeting)
     }
+        @GetMapping("/image")
+    public String image(Model model)  {
+        //String web_server = "http://localhost:8080/";
+        String web_server = "https://csa.nighthawkcodingsociety.com";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "/images/Mona_Lisa.png";
+        lii.add(new ImageInfo(file0, web_server+file0, 12));
+        lii.get(0).read_image();
+
+        String file1 = "/images/bulb_on.gif";
+        lii.add(new ImageInfo(file1, web_server+file1, 2));
+        lii.get(1).read_image();
+
+        String file2 = "/images/bulb_off.png";
+        lii.add(new ImageInfo(file2, web_server+file2, 7));
+        lii.get(2).read_image();
+
+        model.addAttribute("lii", lii);
+        return "starters/image";
+    }
+
+    @GetMapping("/image/grayscale")
+    public String image_grayscale(Model model) {
+        //String web_server = "http://localhost:8080/";
+        String web_server = "https://csa.nighthawkcodingsociety.com";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "/images/Mona_Lisa.png";
+        lii.add(new ImageInfo(file0, web_server+file0, 12));
+        String str = lii.get(0).grayscale();
+//        String str = lii.get(0).grayscale();
+        model.addAttribute("str", str);
+        return "starters/image_grayscale";
+    }
 }
