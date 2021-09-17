5197aa1f187e672731d798bfce48c2d29210ea21
Allen <purplebears321@gmail.com>
2021-09-17 09:53:29 -0700

    DEMORGANS

---
+++ src/main/java/com/example/sping_portfolio/minilabs/DeMorgans.java
@@ -0,0 +1,20 @@
+package com.example.sping_portfolio.minilabs;
+
+public class DeMorgans {
+        public static void main(String[] args) {
+            boolean oversixfoot = false;
+            boolean looksfit = true;
+
+            if (oversixfoot && looksfit){
+                System.out.println("You are like Allen (100% chance)");
+            }
+            else if (!(oversixfoot || !looksfit)){
+                System.out.println("You are Charlie (0% chance)");
+            }
+            else if (oversixfoot || looksfit){
+                System.out.println("You have a 50% chance");
+            }
+        }
+
+    }
+
