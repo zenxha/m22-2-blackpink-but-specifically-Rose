a1135eb5086a3dbd416a56c15c5b303446d19400
zenxha <komays5042@gmail.com>
2021-09-22 10:12:50 -0700

    Allen added html page for image, table not showing thou

--- src/main/java/com/example/sping_portfolio/controllers/Komay.java
+++ src/main/java/com/example/sping_portfolio/controllers/Komay.java
@@ -3,12 +3,14 @@ package com.example.sping_portfolio.controllers;
  * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
  */
 
+import com.example.sping_portfolio.model.ImageInfo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
@@ -34,5 +36,27 @@ public class Komay {
         // model.addAttribute("date", date); // MODEL is passed to html
         return "home/input";
     }
+    @GetMapping("/image")
+    public String image(Model model)  {
+        //String web_server = "http://localhost:8080/";
+        String web_server = "https://localhost:8080/";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.scmp.com%2Fmagazines%2Fstyle%2Fnews-trends%2Farticle%2F3003466%2F4-reasons-why-we-love-lisa-k-pop-girl-group-blackpinks&psig=AOvVaw3rhOrTP8i8NCiu-8J2Jhnn&ust=1632416807795000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCSjNuIk_MCFQAAAAAdAAAAABAI";
+        lii.add(new ImageInfo(file0, web_server+file0, 12));
+        lii.get(0).read_image();
+
+        String file1 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.news18.com%2Fnews%2Fmovies%2Fblackpinks-jisoo-completes-10-years-with-yg-entertainment-blinks-celebrate-4035527.html&psig=AOvVaw27dSolSlyZSMbHWYPLL-oH&ust=1632416865428000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNCsxe2Ik_MCFQAAAAAdAAAAABAN";
+        lii.add(new ImageInfo(file1, web_server+file1, 2));
+        lii.get(1).read_image();
+
+        String file2 = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FRos%25C3%25A9_(singer)&psig=AOvVaw22TstO3ef9JeK1RBsK3HgF&ust=1632416887510000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjUj_iIk_MCFQAAAAAdAAAAABAD";
+        lii.add(new ImageInfo(file2, web_server+file2, 7));
+        lii.get(2).read_image();
+
+        model.addAttribute("lii", lii);
+        return "home/images";
+    }
+
 }
 
