8b8390ee40b0d1432a3ab92a857bd3b039ed37d6
Charlie <trollol678534@gmail.com>
2021-10-13 10:00:37 -0700

    controller page

--- src/main/java/com/example/sping_portfolio/controllers/Charlie.java
+++ src/main/java/com/example/sping_portfolio/controllers/Charlie.java
@@ -9,12 +9,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
-public class Greet4 {
+public class Charlie {
     @GetMapping("/charlie")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String greeting(@RequestParam(name="name", required=false, defaultValue="Charlie") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
         return "individual/charlie"; // returns HTML VIEW (greeting)
     }
-
+    @GetMapping("/song1")
+    public String song() {
+        // model.addAttribute("date", date); // MODEL is passed to html
+        return "home/song";
+    }
 }
